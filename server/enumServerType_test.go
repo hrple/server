e323500090eed2dac7b4b36577917522fc4d8e81
Brett Mostert <brettmostert@gmail.com>
2020-02-29 18:06:02 +0200

    Added tests for enumServerType when providing incorrect running context type

--- server/enumServerType_test.go
+++ server/enumServerType_test.go
@@ -25,3 +25,14 @@ func TestGetRunningContextTypeStandalone(t *testing.T) {
 		t.Fatalf("Error failed to GetRunningContextType - Error: %v", "Expected: "+RunningContextTypeStandalone)
 	}
 }
+
+func TestGetRunningContextTypeRandom(t *testing.T) {
+	runningContextType, err := GetRunningContextType("RunAway")
+	if err == nil {
+		t.Fatal("Error expect when providing invalid RunningContext")
+	}
+
+	if runningContextType != RunningContextTypeStandalone {
+		t.Fatalf("Error expected %v as default instead of %v", RunningContextTypeStandalone, runningContextType)
+	}
+}
