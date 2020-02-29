7443f6bc698ab5697ea00777dc7e010f0b80a028
Brett Mostert <brettmostert@gmail.com>
2020-02-29 16:38:45 +0200

    bash linting fix

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,6 +5,8 @@ golangci_res=$?
 if [ $golangci_res -gt "0" ]
 then
     curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.23.7	    
+else
+    echo $golangci_content
 fi
 
 golint_content=$(golint)
@@ -13,6 +15,8 @@ golint_res=$?
 if [ $golint_res -gt "0" ]
 then
     go get -u golang.org/x/lint/golint
+else
+    echo $golint_content
 fi
 
 safesql_content=$(safesql)
@@ -21,6 +25,8 @@ safesql_res=$?
 if [ $safesql_res -gt "0" ]
 then
     go get github.com/stripe/safesql
+else
+    echo $safesql_content
 fi
 
 git config core.hooksPath .githooks
