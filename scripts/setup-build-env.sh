387bd2adfcd17264b15570249514ea4576d47e65
Brett Mostert <brettmostert@gmail.com>
2020-02-29 14:49:09 +0200

    Adding test coverage

--- scripts/setup-build-env.sh
+++ scripts/setup-build-env.sh
@@ -6,3 +6,12 @@ if [ $golangci_res -gt "0" ]
 then
     curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.23.7	    
 fi
+
+DIR="bin"
+if [ -d "$DIR" ]; then
+  ### Take action if $DIR exists ###
+  echo "bin folder exists..."
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  mkdir bin
+fi
